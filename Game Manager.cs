7cf1426366da32373d7196988b7b6d5c2b4a8ca3
Marciuschextus <111387729+Marciuschextus@users.noreply.github.com>
2025-10-05 00:31:30 -0400

    Add files via upload
    Game Manager with simple game objects for the text descriptions and methods to display and undisplay them that can be called as long as you make a game manager class in your scripts
    Separate scenes with some of the texts to be read

---
+++ Game Manager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    public GameObject tutorialText;
+    public GameObject turbineText;
+    public GameObject primaryLoopText;
+    public GameObject coolantText;
+    public GameObject controlRodText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        tutorialText.SetActive(false);
+        turbineText.SetActive(false);
+        primaryLoopText.SetActive(false);
+        coolantText.SetActive(false);
+        controlRodText.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void setTutorialText()
+    {
+        if (tutorialText.activeSelf)
+        {
+            tutorialText.SetActive(false);
+        }
+        else
+        {
+            tutorialText.SetActive(true);
+        }
+    }
+        public void setTurbineText()
+        {
+            if (turbineText.activeSelf)
+            {
+                turbineText.SetActive(false);
+            }
+            else
+            {
+                turbineText.SetActive(true);
+            }
+        }
+
+        public void setCoolantText()
+        {
+                if (coolantText.activeSelf)
+                {
+                    coolantText.SetActive(false);
+                }
+                else
+                {
+                    coolantText.SetActive(true);
+                }
+        }
+
+        public void setPrimaryLoopText()
+        {
+            if (primaryLoopText.activeSelf)
+            {
+                primaryLoopText.SetActive(false);
+            }
+            else
+            {
+                primaryLoopText.SetActive(true);
+            }
+        }
+    public void setControlRodText()
+    {
+        if (controlRodText.activeSelf)
+        {
+            controlRodText.SetActive(false);
+        }
+        else
+        {
+            controlRodText.SetActive(true);
+        }
+    }
+}
+
+
